b74583af227cda388e050dd1a741170fb09bdf15
Noah <noahhughesph@gmail.com>
2023-06-06 19:37:51 -0400

    all tests passing

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution in this file!
+const distanceFromHqInBlocks = (pickupLocation) => {
+  return Math.abs(42 - pickupLocation);
+};
+
+const distanceFromHqInFeet = (pickupLocation) => {
+  return distanceFromHqInBlocks(pickupLocation) * 264;
+};
+
+const distanceTravelledInFeet = (pickupLocation, dropoffLocation) => {
+  return Math.abs(pickupLocation - dropoffLocation) * 264;
+};
+
+const calculatesFarePrice = (pickupLocation, dropoffLocation) => {
+  const fareDistance = distanceTravelledInFeet(pickupLocation, dropoffLocation);
+  if (fareDistance < 400) {
+    return 0;
+  } else if (fareDistance <= 2000) {
+    return (fareDistance - 400) * 0.02;
+  } else if (fareDistance < 2500) {
+    return 25;
+  }
+  return "cannot travel that far";
+};
